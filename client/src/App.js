65f7e6c45f091f96aaa51a72fbd42908b5441cce
Joshua Alley <jauschalley@gmail.com>
2019-05-01 22:02:15 -0400

    added foreman, knock, and JWT authentication test

--- client/src/App.js
+++ client/src/App.js
@@ -8,12 +8,32 @@ class App extends Component {
     this.state = { bananasReceived: "" }
     this.getBananas = this.getBananas.bind(this)
   }
+  login() {
+    const email = $("#email").val()
+    const password = $("#password").val()
+    const request = { "auth": { "email": email, "password": password } }
+    console.log(request)
+    $.ajax({
+      url: "/user_token",
+      type: "POST",
+      data: request,
+      dataType: "json",
+      success: function (result) {
+        console.log(result)
+        localStorage.setItem("jwt", result.jwt)
+      }
+    })
+  }
   getBananas() {
+    let token = "Bearer " + localStorage.getItem("jwt")
+    console.log(token)
     $.ajax({
       url: "/bananas",
       type: "GET",
+      beforeSend: function (xhr) { xhr.setRequestHeader('Authorization', token) },
       context: this, // Allows us to use this.setState inside success
       success: function (result) {
+        console.log(result)
         this.setState({ bananasReceived: JSON.stringify(result) })
       }
     })
@@ -21,9 +41,36 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <h1 style={{ marginTop: "20vh", marginBottom: "5vh" }}>
+          Banana Management System
+        </h1>
+        <form>
+          <label htmlFor="email">Email: </label>
+          <br />
+          <input
+            name="email"
+            id="email"
+            type="email"
+          />
+          <br /><br />
+          <label htmlFor="password">Password:</label>
+          <br />
+          <input
+            name="password"
+            id="password"
+            type="password"
+          />
+        </form>
+        <br />
+        <button
+          onClick={this.login}
+        >
+          Login
+          </button>
+        <br />
         <button
           onClick={this.getBananas}
-          style={{ marginTop: '25vh' }}
+          style={{ marginTop: "10vh" }}
         >
           Get Bananas
         </button>
